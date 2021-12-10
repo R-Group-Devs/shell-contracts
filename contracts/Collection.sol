4f3e76568332cce052cac63b5599dce474af798c
Brandon Valosek <bvalosek@gmail.com>
2021-12-10 16:06:26 -0600

    add engine broadcast fn

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -9,7 +9,15 @@ import "./IEngine.sol";
 contract Collection is Ownable, ERC721, IERC2981 {
   IEngine public engine;
 
-  event EngineInstalled(address indexed from, IEngine indexed engine);
+  // emitted whenever a new engine is installed
+  event EngineInstalled(IEngine indexed engine);
+
+  // an emit proxied from the installed engine
+  event EngineBroadcast(
+    string indexed topic,
+    uint256 indexed aParam,
+    uint256 indexed bParam,
+    string data);
 
   mapping(uint256 => string) public tokenData;
 
@@ -17,6 +25,11 @@ contract Collection is Ownable, ERC721, IERC2981 {
     engine = engine_;
     _transferOwnership(owner);
     _installEngine(engine_);
+  }
+
+  modifier onlyEngine() {
+    require(_msgSender() == address(engine), "caller must be installed engine");
+    _;
   }
 
   // swap out the current engine. Can only be called by owner
@@ -30,10 +43,14 @@ contract Collection is Ownable, ERC721, IERC2981 {
   }
 
   // mint a new token. Can only be called by the currently installed engine
-  function mint(address to, uint256 tokenId, string memory data) external {
-    require(msg.sender == address(engine), "mint must be called by engine");
+  function mint(address to, uint256 tokenId, string memory data) external onlyEngine {
     _mint(to, tokenId);
     tokenData[tokenId] = data;
+  }
+
+  // Broadcast an event. Can only be called by the currently installed engine
+  function broadcast(string calldata topic, uint256 a, uint256 b, string calldata data) external onlyEngine {
+    emit EngineBroadcast(topic, a, b, data);
   }
 
   // delegate resolution of royaltyInfo to the currently installed engine
@@ -54,7 +71,7 @@ contract Collection is Ownable, ERC721, IERC2981 {
 
   function _installEngine(IEngine engine_) internal {
     engine = engine_;
-    emit EngineInstalled(msg.sender, engine_);
+    emit EngineInstalled(engine_);
   }
 
 }
