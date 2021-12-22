d038e244c8a33a449473903a181b94a9e2d2e5ff
Brandon Valosek <bvalosek@gmail.com>
2021-12-22 14:17:11 -0600

    update tests

--- test/CollectionFactory.spec.ts
+++ test/CollectionFactory.spec.ts
@@ -4,8 +4,8 @@ import { ethers } from "hardhat";
 import {
   ShellFactory,
   ShellERC721,
-  ShellERC721__factory,
   MockEngine,
+  ShellERC721__factory,
 } from "../typechain";
 
 describe("CollectionFactory", function () {
@@ -13,23 +13,26 @@ describe("CollectionFactory", function () {
   // fixtures
   // ---
 
-  let Collection: ShellERC721__factory;
+  let ShellERC721: ShellERC721__factory;
+  let erc721: ShellERC721;
   let factory: ShellFactory;
   let mockEngine: MockEngine;
   let accounts: SignerWithAddress[];
   let a0: string, a1: string, a2: string, a3: string;
 
   beforeEach(async () => {
-    Collection = await ethers.getContractFactory("ShellERC721");
+    ShellERC721 = await ethers.getContractFactory("ShellERC721");
     const ShellFactory = await ethers.getContractFactory("ShellFactory");
     const MockEngine = await ethers.getContractFactory("MockEngine");
-    [factory, mockEngine, accounts] = await Promise.all([
+    [erc721, factory, mockEngine, accounts] = await Promise.all([
+      ShellERC721.deploy(),
       ShellFactory.deploy(),
       MockEngine.deploy(),
       ethers.getSigners(),
     ]);
     // give infinite allowance for a0 to hv
     [a0, a1, a2, a3] = accounts.map((a) => a.address);
+    await factory.registerImplementation("erc721", erc721.address);
   });
 
   const createCollection = async (
@@ -38,10 +41,16 @@ describe("CollectionFactory", function () {
     engine = mockEngine.address,
     owner = a0
   ): Promise<ShellERC721> => {
-    const trx = await factory.createCollection(name, symbol, engine, owner);
+    const trx = await factory.createCollection(
+      name,
+      symbol,
+      "erc721",
+      engine,
+      owner
+    );
     const mined = await trx.wait();
     const address = mined.events?.[2].args?.collection;
-    const collection = Collection.attach(address);
+    const collection = ShellERC721.attach(address);
     return collection;
   };
 
