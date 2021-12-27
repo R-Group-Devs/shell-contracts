448e300c2506b4248880f821808e1e77dd580b48
Brandon Valosek <bvalosek@gmail.com>
2021-12-27 15:22:37 -0600

    update pasta script

--- copy-pasta-abis.sh
+++ copy-pasta-abis.sh
@@ -3,8 +3,9 @@
 # script to copy ABIs from this repo to sibling repos (subgraph and frontend)
 
 BASE=./artifacts/contracts
-GRAPH=../shell-subgraph/abis/
-FRONTEND=../shell-frontend/src/shell/abis/
+OZ_BASE=./artifacts/@openzeppelin/contracts-upgradeable
+GRAPH=../shell-subgraph/abis
+FRONTEND=../shell-frontend/src/shell/abis
 
 yarn build \
   && cp \
@@ -13,6 +14,8 @@ yarn build \
     $BASE/IShellERC721.sol/IShellERC721.json \
     $BASE/IShellERC1155.sol/IShellERC1155.json \
     $BASE/IShellFactory.sol/IShellFactory.json \
+    $OZ_BASE/token/ERC721/IERC721Upgradeable.sol/IERC721Upgradeable.json \
+    $OZ_BASE/token/ERC1155/IERC1155Upgradeable.sol/IERC1155Upgradeable.json \
       $GRAPH \
   && cp \
     $BASE/IEngine.sol/IEngine.json \
