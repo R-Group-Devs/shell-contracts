4ace55f478d6cf5be84bf2f0a6fab9b16d91a62a
Brandon Valosek <bvalosek@gmail.com>
2022-02-16 08:27:29 -0600

    header

--- contracts/ShellFactory.sol
+++ contracts/ShellFactory.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 /*
-                  ShellFactory V1
+                   ShellFactory V1
 
         ███████╗██╗  ██╗███████╗██╗     ██╗
         ██╔════╝██║  ██║██╔════╝██║     ██║
